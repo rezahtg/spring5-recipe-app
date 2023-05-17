92cf0515cc0130f8728e42763ee1de1352db8f67
reza hutagaol <silentsound05@gmai.com>
2023-05-17 16:09:12 +0700

    intial commit

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String getIndex(Model model){
+
+        model.addAttribute("index",null);
+
+        return "index";
+    }
+
+}
---
+++ src/main/java/guru/springframework/models/Receipe.java
@@ -0,0 +1,17 @@
+package guru.springframework.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Receipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String receipeDesc;
+
+}
