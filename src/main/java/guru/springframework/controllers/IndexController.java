581bc87d41723b373ae693c8216589ba3f834136
reza hutagaol <silentsound05@gmai.com>
2023-05-22 18:20:32 +0700

    trying deploy

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,16 +1,35 @@
 package guru.springframework.controllers;
 
+import guru.springframework.models.Category;
+import guru.springframework.models.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @GetMapping("/")
     public String getIndex(Model model){
 
         model.addAttribute("index",null);
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id: " + categoryOptional.get().getId());
+        System.out.println("UOM  id: " + unitOfMeasureOptional.get().getId());
 
         return "index";
     }
